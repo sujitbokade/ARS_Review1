2ee2657c7f5e7cc48afa13c3cc86e2fd022cc8b4
sujit <sujitbokade2014@gmail.com>
2022-08-01 17:54:40 +0530

    [Sujit]Fibonacci Series Program

---
+++ src/com/bridgelabz/basic/FibonacciSeries.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.basic;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n=10;
+        int x = 0, y = 1, z = 0, count = 1;
+        while (count <= n) {
+            System.out.println(z + "   ");
+            z = x + y;
+            y = x;
+            x = z;
+            count++;
+
+        }
+
+    }
+}
